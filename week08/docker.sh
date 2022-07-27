e14e751411b095c9c1ecefc8ff2ca030d559112e
Lachlan <lachlanwaugh8@gmail.com>
2022-07-27 15:21:13 +1000

    wait, actually fixed up the docker.sh to check if docker images exists, woops

--- week08/docker.sh
+++ week08/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$(docker image inspect plsiamlegit/6447-ubuntu 2> /dev/null)" == "" ]]; then
+if [[ "$(docker image inspect plsiamlegit/6447-ubuntu:pwndocker 2> /dev/null)" == "[]" ]]; then
     docker run -d --rm -h banana --name banana -v $(pwd):/ctf/work --cap-add=SYS_PTRACE plsiamlegit/6447-ubuntu:pwndocker
 fi
 
